ca906fa2ea2a44fb38095b3aed03a03e882338eb
Kushwanth Ram <kushwanth@Kushwanths-MacBook-Pro.local>
2025-03-01 12:01:45 -0600

    Browser Use agents

---
+++ backend/browser/invoke_browser.py
@@ -0,0 +1,23 @@
+from langchain_openai import ChatOpenAI
+from browser_use import Agent
+import asyncio
+from dotenv import load_dotenv
+load_dotenv()
+
+class BrowserUse:
+    def __init__(self, open_ai_key: str):
+        self.open_ai_key = open_ai_key
+
+    def InvokeBrowserAgent(self, task: str):
+        try:
+            os.environ["OPENAI_API_KEY"] = self.openai_key
+            agent = Agent(
+                task=task,
+                llm=ChatOpenAI(model="gpt-4o"),
+            )
+            result = await agent.run()
+            return result
+        except Exception as e:
+            print(e)
+        finally:
+            os.environ.pop("OPENAI_API")
